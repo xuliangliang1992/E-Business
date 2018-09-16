2c8b9f54f5f4c3408972ec13ac29f095b2d4dc57
xuliangliang <1299945061@qq.com>
2018-09-16 23:14:57 +0800

    首页引入淘宝vLayout布局

--- app/src/main/java/com/jinlong/ebusiness/func/home/HomeFragment.java
+++ app/src/main/java/com/jinlong/ebusiness/func/home/HomeFragment.java
@@ -1,13 +1,30 @@
 package com.jinlong.ebusiness.func.home;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.alibaba.android.vlayout.DelegateAdapter;
+import com.alibaba.android.vlayout.VirtualLayoutManager;
+import com.alibaba.android.vlayout.layout.LinearLayoutHelper;
+import com.alibaba.android.vlayout.layout.SingleLayoutHelper;
+import com.alibaba.android.vlayout.layout.StickyLayoutHelper;
+import com.jinlong.ebusiness.R;
 import com.jinlong.ebusiness.base.BaseFragment;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * @author xll
@@ -15,6 +32,16 @@ import com.jinlong.ebusiness.base.BaseFragment;
  */
 
 public class HomeFragment extends BaseFragment {
+
+    @BindView(R.id.rv_home)
+    RecyclerView recyclerView;
+    Unbinder unbinder;
+
+    private ArrayList<HashMap<String, Object>> listItem;
+    MyAdapter Adapter_linearLayout,Adapter_GridLayout,Adapter_FixLayout,Adapter_ScrollFixLayout
+            ,Adapter_FloatLayout,Adapter_ColumnLayout,Adapter_SingleLayout,Adapter_onePlusNLayout,
+            Adapter_StickyLayout,Adapter_StaggeredGridLayout;
+
     public static HomeFragment newInstance() {
         return new HomeFragment();
     }
@@ -27,6 +54,171 @@ public class HomeFragment extends BaseFragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.home_fragment, container, false);
+        unbinder = ButterKnife.bind(this, view);
+        VirtualLayoutManager layoutManager = new VirtualLayoutManager(getActivity());
+        // 创建VirtualLayoutManager对象
+        // 同时内部会创建一个LayoutHelperFinder对象，用来后续的LayoutHelper查找
+
+        recyclerView.setLayoutManager(layoutManager);
+        // 将VirtualLayoutManager绑定到recyclerView
+
+        /**
+         * 步骤2：设置组件复用回收池
+         * */
+        RecyclerView.RecycledViewPool viewPool = new RecyclerView.RecycledViewPool();
+        recyclerView.setRecycledViewPool(viewPool);
+        viewPool.setMaxRecycledViews(0, 10);
+
+
+        /**
+         * 步骤3:设置需要存放的数据
+         * */
+        listItem = new ArrayList<HashMap<String, Object>>();
+        for (int i = 0; i < 100; i++) {
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            map.put("ItemTitle", "第" + i + "行");
+            map.put("ItemImage", R.mipmap.ic_launcher);
+            listItem.add(map);
+
+        }
+
+        /**
+         设置线性布局
+         */
+        LinearLayoutHelper linearLayoutHelper = new LinearLayoutHelper();
+        // 创建对应的LayoutHelper对象
+
+        // 公共属性
+        linearLayoutHelper.setItemCount(10);// 设置布局里Item个数
+        linearLayoutHelper.setPadding(20, 20, 20, 20);// 设置LayoutHelper的子元素相对LayoutHelper边缘的距离
+        linearLayoutHelper.setMargin(20, 20, 20, 20);// 设置LayoutHelper边缘相对父控件（即RecyclerView）的距离
+        // linearLayoutHelper.setBgColor(Color.GRAY);// 设置背景颜色
+        linearLayoutHelper.setAspectRatio(6);// 设置设置布局内每行布局的宽与高的比
+
+        // linearLayoutHelper特有属性
+        linearLayoutHelper.setDividerHeight(10);
+        // 设置间隔高度
+        // 设置的间隔会与RecyclerView的addItemDecoration（）添加的间隔叠加.
+
+        linearLayoutHelper.setMarginBottom(100);
+        // 设置布局底部与下个布局的间隔
+
+        // 创建自定义的Adapter对象 & 绑定数据 & 绑定对应的LayoutHelper进行布局绘制
+        Adapter_linearLayout  = new MyAdapter(getActivity(), linearLayoutHelper, 10, listItem) {
+            // 参数2:绑定绑定对应的LayoutHelper
+            // 参数3:传入该布局需要显示的数据个数
+            // 参数4:传入需要绑定的数据
+
+            // 通过重写onBindViewHolder()设置更丰富的布局效果
+            @Override
+            public void onBindViewHolder(MainViewHolder holder, int position) {
+                super.onBindViewHolder(holder, position);
+                // 为了展示效果,将布局的第一个数据设置为linearLayout
+                if (position == 0) {
+                    holder.Text.setText("Linear");
+                }
+
+                //为了展示效果,将布局里不同位置的Item进行背景颜色设置
+                if (position < 2) {
+                    holder.itemView.setBackgroundColor(0x66cc0000 + (position - 6) * 128);
+                } else if (position % 2 == 0) {
+                    holder.itemView.setBackgroundColor(0xaa22ff22);
+                } else {
+                    holder.itemView.setBackgroundColor(0xccff22ff);
+                }
+
+
+            }
+        };
+        /**
+         设置吸边布局
+         */
+        StickyLayoutHelper stickyLayoutHelper = new StickyLayoutHelper();
+
+        // 公共属性
+        stickyLayoutHelper.setItemCount(3);// 设置布局里Item个数
+        stickyLayoutHelper.setPadding(20, 20, 20, 20);// 设置LayoutHelper的子元素相对LayoutHelper边缘的距离
+        stickyLayoutHelper.setMargin(20, 20, 20, 20);// 设置LayoutHelper边缘相对父控件（即RecyclerView）的距离
+        stickyLayoutHelper.setBgColor(Color.GRAY);// 设置背景颜色
+        stickyLayoutHelper.setAspectRatio(3);// 设置设置布局内每行布局的宽与高的比
+
+        // 特有属性
+        stickyLayoutHelper.setStickyStart(true);
+        // true = 组件吸在顶部
+        // false = 组件吸在底部
+
+        stickyLayoutHelper.setOffset(100);// 设置吸边位置的偏移量
+
+        Adapter_StickyLayout = new MyAdapter(getActivity(), stickyLayoutHelper,1, listItem) {
+            // 设置需要展示的数据总数,此处设置是1
+            // 为了展示效果,通过重写onBindViewHolder()将布局的第一个数据设置为Stick
+            @Override
+            public void onBindViewHolder(MainViewHolder holder, int position) {
+                super.onBindViewHolder(holder, position);
+                if (position == 0) {
+                    holder.Text.setText("Stick");
+                }
+            }
+        };
+
+        SingleLayoutHelper singleLayoutHelper = new SingleLayoutHelper();
+        // 公共属性
+        singleLayoutHelper.setItemCount(3);// 设置布局里Item个数
+        singleLayoutHelper.setPadding(20, 20, 20, 20);// 设置LayoutHelper的子元素相对LayoutHelper边缘的距离
+        singleLayoutHelper.setMargin(20, 20, 20, 20);// 设置LayoutHelper边缘相对父控件（即RecyclerView）的距离
+        singleLayoutHelper.setBgColor(Color.GRAY);// 设置背景颜色
+        singleLayoutHelper.setAspectRatio(6);// 设置设置布局内每行布局的宽与高的比
+
+
+        Adapter_SingleLayout = new MyAdapter(getActivity(), singleLayoutHelper,1, listItem) {
+            // 设置需要展示的数据总数,此处设置是1
+            // 为了展示效果,通过重写onBindViewHolder()将布局的第一个数据设置为Single
+            @Override
+            public void onBindViewHolder(MainViewHolder holder, int position) {
+                super.onBindViewHolder(holder, position);
+                if (position == 0) {
+                    holder.Text.setText("Single");
+                }
+            }
+        };
+        /**
+         *步骤5:将生成的LayoutHelper 交给Adapter，并绑定到RecyclerView 对象
+         **/
+
+        // 1. 设置Adapter列表（同时也是设置LayoutHelper列表）
+        List<DelegateAdapter.Adapter> adapters = new LinkedList<>();
+
+        // 2. 将上述创建的Adapter对象放入到DelegateAdapter.Adapter列表里
+        adapters.add(Adapter_StickyLayout) ;
+        adapters.add(Adapter_SingleLayout) ;
+        adapters.add(Adapter_linearLayout) ;
+        //        adapters.add(Adapter_ScrollFixLayout) ;
+        //        adapters.add(Adapter_GridLayout) ;
+        //        adapters.add(Adapter_FixLayout) ;
+        //        adapters.add(Adapter_FloatLayout) ;
+        //        adapters.add(Adapter_ColumnLayout) ;
+//        adapters.add(Adapter_onePlusNLayout) ;
+//        adapters.add(Adapter_StaggeredGridLayout) ;
+
+
+        //
+        // 3. 创建DelegateAdapter对象 & 将layoutManager绑定到DelegateAdapter
+        DelegateAdapter delegateAdapter = new DelegateAdapter(layoutManager);
+
+        // 4. 将DelegateAdapter.Adapter列表绑定到DelegateAdapter
+        delegateAdapter.setAdapters(adapters);
+
+        // 5. 将delegateAdapter绑定到recyclerView
+        recyclerView.setAdapter(delegateAdapter);
+
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
---
+++ app/src/main/java/com/jinlong/ebusiness/func/home/MyItemClickListener.java
@@ -0,0 +1,10 @@
+package com.jinlong.ebusiness.func.home;
+
+import android.view.View;
+
+/**
+ * Created by Carson_Ho on 17/4/26.
+ */
+public interface MyItemClickListener {
+    public void onItemClick(View view, int postion);
+}
