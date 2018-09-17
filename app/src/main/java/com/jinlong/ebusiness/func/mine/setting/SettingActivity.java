7d6e546a9b1ac8f8675567d189b616bc4524715a
xuliangliang <1299945061@qq.com>
2018-09-17 21:23:54 +0800

    设置页面大致样式

---
+++ app/src/main/java/com/jinlong/ebusiness/func/mine/setting/SettingActivity.java
@@ -0,0 +1,21 @@
+package com.jinlong.ebusiness.func.mine.setting;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.jinlong.ebusiness.R;
+import com.jinlong.ebusiness.base.BaseActivity;
+
+/**
+ * @author xll
+ * @date 2018/9/17
+ */
+
+public class SettingActivity extends BaseActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.mine);
+    }
+}
---
+++ app/src/main/java/com/jinlong/ebusiness/func/mine/setting/SettingFragment.java
@@ -0,0 +1,35 @@
+package com.jinlong.ebusiness.func.mine.setting;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jinlong.ebusiness.R;
+import com.jinlong.ebusiness.base.BaseFragment;
+
+/**
+ * @author xll
+ * @date 2018/9/17
+ */
+
+public class SettingFragment extends BaseFragment {
+    public static SettingFragment newInstance() {
+        return new SettingFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.setting_fragment, container, false);
+
+        return view;
+    }
+}
